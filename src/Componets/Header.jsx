747c8627bf9707b458be894496560ed0484814e2
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-04-29 11:33:41 +0200

    Refactor Header component layout; adjust logo size and container styling

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -6,10 +6,10 @@ export default function Header() {
     return (
         <>
             <nav className="navbar navbar-expand navbar-light bg-light shadow-sm">
-                <div className="container-fluid">
-                    <div className="d-flex align-items-center">
-                        <Link className="nav-item nav-link active mx-3" to="/">
-                            <img src="/images/BooRoad.png" alt="Logo" style={{ width: "80px" }} />
+                <div className="container">
+                    <div className="py-2">
+                        <Link className="nav-item nav-link active" to="/">
+                            <img src="/images/BooRoad.png" alt="Logo" style={{ width: "60px" }} />
                         </Link>
 
                         {/* <Link to="/add-travel" className="nav-link text-dark add-travel">
