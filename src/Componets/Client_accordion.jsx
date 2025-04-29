b2b83b5b9da7436dca1d67873b63d0b911acba0a
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-04-29 20:40:57 +0200

    Add clipboard copy functionality for codice fiscale; update styles for copy and email buttons

--- src/Componets/Client_accordion.jsx
+++ src/Componets/Client_accordion.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
 export default function Clientaccordion({ client }) {
+
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(client.codiceFiscale).then(() => {
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000);
+        });
+    };
     return (
 
         <div className="accordion rounded" id="accordionFlushExample">
@@ -10,19 +21,37 @@ export default function Clientaccordion({ client }) {
                 </h2>
                 <div id={`collapse${client.id}`} className="accordion-collapse collapse" aria-labelledby={`heading${client.id}`}>
                     <div className="accordion-body">
-                        <p><strong>Codice Fiscale:</strong> {client.codiceFiscale}</p>
+                        <p>
+                            <strong>Codice Fiscale:</strong> {client.codiceFiscale}
+                            <button
+                                onClick={handleCopy}
+                                className="call-btn copy-btn"
+                                title="Copia codice fiscale">
+                                <i className="bi bi-clipboard"></i>
+                            </button>
+                            {copied && <span className={`ms-2 copied-message ${copied ? 'visible' : ''}`}>
+                                Copiato!
+                            </span>}
+                        </p>
+
                         <p>
                             <strong>Cellulare:</strong> {client.phone}
                             <a
                                 href={`tel:${client.phone}`}
                                 className="call-btn"
-                                title="Chiama"
-                            >
+                                title="Chiama">
                                 <i className="bi bi-telephone"></i>
-                                <span style={{ marginLeft: '4px' }}>Chiama</span>
                             </a>
                         </p>
-                        <p><strong>Email:</strong> {client.email}</p>
+                        <p>
+                            <strong>Email:</strong> {client.email}
+                            <a
+                                href={`mailto:${client.email}`}
+                                className="call-btn email-btn"
+                                title="Email">
+                                <i className="bi bi-envelope"></i>
+                            </a>
+                        </p>
                     </div>
                 </div>
             </div>
